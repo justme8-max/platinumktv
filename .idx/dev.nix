a235326b6de8fde995d2aaba8b175817ecdc3358
bitbuddy99 <bitbuddy99@gmail.com>
2025-10-25 05:15:07 +0000

    Commit

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -4,24 +4,22 @@
     pkgs.nodejs_20
   ];
   idx.extensions = [
-    "svelte.svelte-vscode"
+    "svelte.svelte-vscode",
     "vue.volar"
   ];
   idx.previews = {
-    previews = {
-      web = {
-        command = [
-          "npm"
-          "run"
-          "dev"
-          "--"
-          "--port"
-          "$PORT"
-          "--host"
-          "0.0.0.0"
-        ];
-        manager = "web";
-      };
+    web = {
+      command = [
+        "npm"
+        "run"
+        "dev"
+        "--"
+        "--port"
+        "$PORT"
+        "--host"
+        "0.0.0.0"
+      ];
+      manager = "web";
     };
   };
 }
